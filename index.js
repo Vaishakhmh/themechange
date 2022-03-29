fe54b074a8e496bae065e21547274b4953362d39
Vaishakh mh <vaishakh.m.h@gmail.com>
2022-03-29 11:09:13 +0530

    deploy

--- index.js
+++ index.js
@@ -8,7 +8,20 @@ const app=express();
 app.use(express.static(path.resolve(__dirname, "./client/fornt/build")));
 app.use(cors())
 app.use(bodyParser());
+
+
+if (process.env.NODE_ENV === "production") {
+    // Add custom routes before JSON Server router
+    server.get("*", (req, res) => {
+      res.sendFile(
+        path.resolve(__dirname, "../", "client", "build", "index.html")
+      );
+    });
+  }
+
+
 app.use(userRoute);
+
 const mongoose=require('mongoose');
 const config = require('./config');
 const connect=mongoose.connect(encodeURI(config.connectionUrl),{connectTimeoutMS:1000}).then(()=>{
