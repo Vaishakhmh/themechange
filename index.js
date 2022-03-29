f2f1834239fc68fb4c1bc664ed68dd6fa2265aea
Vaishakh mh <vaishakh.m.h@gmail.com>
2022-03-29 11:13:33 +0530

    check

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser());
 
 if (process.env.NODE_ENV === "production") {
     // Add custom routes before JSON Server router
-    server.get("*", (req, res) => {
+    app.get("*", (req, res) => {
       res.sendFile(
         path.resolve(__dirname, "../", "client", "build", "index.html")
       );
